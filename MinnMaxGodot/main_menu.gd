d51f054eb903a5f3dcc449dc3d4286ddbed7f71d
RichardHayes <rhshadowgreen@gmail.com>
2023-10-28 10:09:43 -0700

    Add more info page

--- MinnMaxGodot/main_menu.gd
+++ MinnMaxGodot/main_menu.gd
@@ -18,3 +18,6 @@ func _on_button_pressed():
 		$fire/flames.play("fire")
 		$fire/PointLight2D.set_visible(true)
 		$fire/PointLight2D.startTween()
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://more_info.tscn")
